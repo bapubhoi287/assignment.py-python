66c4d47a0389f6080df5685ba3631ad360db12be
Shankar <bapubhoi287@gmail.com>
2022-11-14 10:28:30 +0530

    assignment11 upload

--- 10.py
+++ 10.py
@@ -1,4 +1,4 @@
-#write a python script to print the octal equivalent of a given decimal number.(do not use oct()method)
+#write a python script to print the octal equivalent of a given decimal number.
 n=int(input("enter a number:"))
 m=oct(n)
 print(m)
--- 2.py
+++ 2.py
@@ -4,5 +4,5 @@ sum=0
 for i in range(1,n+1,1):
     sq=i*i
     sum=sum+sq
-print("sum of first natural numbers squars is:",sum,end=' ')
+print("sum of first natural numbers squares is:",sum,end=' ')
 print()
